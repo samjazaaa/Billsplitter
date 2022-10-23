b50e5a412fb82442cf998e4d8fa14affd24b40f4
Felix Knoerr <felix.knoerr@gmx.de>
2022-10-23 11:38:30 +0200

    setup linting and auto format

--- src/SplittingCard.tsx
+++ src/SplittingCard.tsx
@@ -1,57 +1,73 @@
 import { useRef } from "react";
-import { Paper, Typography, List, ListItem, ListItemIcon, ListItemText, TextField, TextFieldProps } from "@mui/material";
-import { DeleteForeverOutlined } from '@material-ui/icons';
+import {
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  TextField,
+  TextFieldProps,
+} from "@mui/material";
+import { DeleteForeverOutlined } from "@material-ui/icons";
 
 type SplittingCardProps = {
-    name: string,
-    values: number[],
-    setValues: (values: number[]) => void,
-}
+  name: string;
+  values: number[];
+  setValues: (values: number[]) => void;
+};
 
 const SplittingCard = ({ name, values, setValues }: SplittingCardProps) => {
+  const inputRef = useRef<TextFieldProps>(null);
 
-    const inputRef = useRef<TextFieldProps>(null);
-
-    const handleAddValue = (event: React.KeyboardEvent<HTMLElement>) => {
-        const inputComponent = inputRef.current;
-        if (event.key === 'Enter' && inputComponent !== null) {
-            const input = inputComponent.value;
-            if (typeof input === 'string') {
-                const val: number = parseFloat(input.replace(/,/, '.').replace('€', ''));
-                if (!isNaN(val)) {
-                    setValues([...values, val]);
-                    inputComponent.value = '';
-                }
-            }
+  const handleAddValue = (event: React.KeyboardEvent<HTMLElement>) => {
+    const inputComponent = inputRef.current;
+    if (event.key === "Enter" && inputComponent !== null) {
+      const input = inputComponent.value;
+      if (typeof input === "string") {
+        const val: number = parseFloat(
+          input.replace(/,/, ".").replace("€", "")
+        );
+        if (!isNaN(val)) {
+          setValues([...values, val]);
+          inputComponent.value = "";
         }
-    };
+      }
+    }
+  };
 
-    const handleRemoveValue = (index: number) => {
-        setValues([...values.slice(0, index), ...values.slice(index + 1)]);
-    };
+  const handleRemoveValue = (index: number) => {
+    setValues([...values.slice(0, index), ...values.slice(index + 1)]);
+  };
 
-    return (
-        <Paper
-            sx={{
-                height: 340,
-                width: 210,
-            }}
-        >
-            <Typography variant='h4' align='center' gutterBottom>{name}</Typography>
-            <List dense>
-                {values.map((price, index) => (
-                    <ListItem key={index}>
-                        <ListItemIcon onClick={() => handleRemoveValue(index)}>
-                            <DeleteForeverOutlined />
-                        </ListItemIcon>
-                        <ListItemText>{price}€</ListItemText>
-                    </ListItem>
-                ))}
-            </List>
-            <TextField id={name.toLowerCase()} label={'Add ' + name + ' expense'} onKeyUp={handleAddValue} inputRef={inputRef} />
-        </Paper>
-    );
-
+  return (
+    <Paper
+      sx={{
+        height: 340,
+        width: 210,
+      }}
+    >
+      <Typography variant="h4" align="center" gutterBottom>
+        {name}
+      </Typography>
+      <List dense>
+        {values.map((price, index) => (
+          <ListItem key={index}>
+            <ListItemIcon onClick={() => handleRemoveValue(index)}>
+              <DeleteForeverOutlined />
+            </ListItemIcon>
+            <ListItemText>{price}€</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+      <TextField
+        id={name.toLowerCase()}
+        label={"Add " + name + " expense"}
+        onKeyUp={handleAddValue}
+        inputRef={inputRef}
+      />
+    </Paper>
+  );
 };
 
 export default SplittingCard;
